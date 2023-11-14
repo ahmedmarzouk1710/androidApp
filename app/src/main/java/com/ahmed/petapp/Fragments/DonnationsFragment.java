7a09ce124dace65b2c91c33be49ae56c17f1e7e5
ahmedmarzouk1710 <amarzouk880@gmail.com>
2023-11-14 01:19:48 +0100

    donnation

--- app/src/main/java/com/ahmed/petapp/Fragments/DonnationsFragment.java
+++ app/src/main/java/com/ahmed/petapp/Fragments/DonnationsFragment.java
@@ -4,10 +4,20 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
+import com.ahmed.petapp.Database.AppDatabase;
+import com.ahmed.petapp.Module.Donation;
 import com.example.petapp.R;
 
 /**
@@ -16,7 +26,6 @@ import com.example.petapp.R;
  * create an instance of this fragment.
  */
 public class DonnationsFragment extends Fragment {
-
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -25,6 +34,14 @@ public class DonnationsFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+    private RadioGroup rgDonationAmount;
+    private EditText etDonationCustom;
+    private Button btnDonate;
+    private ProgressBar pbDonationProgress;
+    private ImageView ivDonationImpact;
+
+    private int selectedDonationAmount = 10;
 
     public DonnationsFragment() {
         // Required empty public constructor
@@ -61,6 +78,87 @@ public class DonnationsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_donnations, container, false);
+        View view = inflater.inflate(R.layout.fragment_donnations, container, false);
+        rgDonationAmount = view.findViewById(R.id.rg_donation_amount);
+        etDonationCustom = view.findViewById(R.id.et_donation_custom);
+        btnDonate = view.findViewById(R.id.btn_donate);
+        pbDonationProgress = view.findViewById(R.id.pb_donation_progress);
+        ivDonationImpact = view.findViewById(R.id.iv_donation_impact);
+
+        // Set default radio button selection
+        rgDonationAmount.check(R.id.rb_donation_10);
+
+        // Set up event listeners
+        rgDonationAmount.setOnCheckedChangeListener(donationAmountChangeListener);
+        etDonationCustom.addTextChangedListener(donationAmountTextWatcher);
+        btnDonate.setOnClickListener(donationButtonClickListener);
+
+        return view;
     }
+
+    // RadioGroup listener to handle donation amount selection
+    private RadioGroup.OnCheckedChangeListener donationAmountChangeListener = new RadioGroup.OnCheckedChangeListener() {
+        @Override
+        public void onCheckedChanged(RadioGroup group, int checkedId) {
+            if (checkedId == R.id.rb_donation_10) {
+                selectedDonationAmount = 10;
+            } else if (checkedId == R.id.rb_donation_25) {
+                selectedDonationAmount = 25;
+            } else if (checkedId == R.id.rb_donation_50) {
+                selectedDonationAmount = 50;
+            } else if (checkedId == R.id.rb_donation_100) {
+                selectedDonationAmount = 100;
+            }
+        }
+    };
+
+    private TextWatcher donationAmountTextWatcher = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+            // Check if input is a valid decimal number
+            if (!s.toString().isEmpty()) {
+                try {
+                    double customAmount = Double.parseDouble(s.toString());
+                    if (customAmount < 1) {
+                        etDonationCustom.setError("Invalid donation amount");
+                    } else {
+                        selectedDonationAmount = (int) customAmount;
+                    }
+                } catch (NumberFormatException e) {
+                    etDonationCustom.setError("Invalid donation amount");
+                }
+            } else {
+                selectedDonationAmount = 0; // Reset to default amount if input is empty
+            }
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+        }
+    };
+
+    // Button listener to handle donation processing
+    private View.OnClickListener donationButtonClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            // Simulate donation processing
+            pbDonationProgress.setVisibility(View.VISIBLE);
+            ivDonationImpact.setVisibility(View.VISIBLE);
+
+            // Create a new Donation object
+            Donation donation = new Donation("John Doe", "Money", String.valueOf(selectedDonationAmount), "Credit Card", "2023-11-14");
+
+            // Insert the donation into the database
+            AppDatabase.getAppDatabase(getContext()).donationDao().insertDonation(donation);
+
+            // Update UI after simulated donation processing
+            Toast.makeText(getContext(), "Donation successful: $" + selectedDonationAmount, Toast.LENGTH_SHORT).show();
+            pbDonationProgress.setVisibility(View.GONE);
+            ivDonationImpact.setVisibility(View.GONE);
+        }
+    };
 }
